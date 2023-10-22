7528ce772af95ffbd6502988acb20d1d0f84f675
Grzegorz Patyk <grzegorz.patyk@ttms.pl>
2023-10-22 22:12:01 +0200

    move necesarry components from main branch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Circle from "../lib/Circle.svelte";
+
+	const arr = Array.from(Array(50).keys());
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+    background: black;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
+
+<svelte:head>
+ <style>
+   body {
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background: black;
+    perspective: 800px;
+  }
+ </style>
+</svelte:head>
+
+<main>
+	{#each arr as number}
+    <Circle delay={number * 0.15} radius={50 * number} />
+  {/each}
+</main>
